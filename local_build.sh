3ee8bf13bebe75863c71b71d9d1c9e71063b50a1
Yohei Kakiuchi <kakiuchi.yohei.sw@tut.jp>
2024-08-31 16:32:08 +0900

    for building latest xeus

--- local_build.sh
+++ local_build.sh
@@ -4,10 +4,15 @@ set -e
 
 OUTPUT_DIR=/opt/xeus3
 
+(mkdir json && wget https://github.com/nlohmann/json/archive/refs/tags/v3.11.2.tar.gz --quiet -O - | tar zxf - --strip-components 1 -C json)
+(cd json; cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=${OUTPUT_DIR} . ; make install -j$(nproc) )
+
 (mkdir xtl && wget https://github.com/xtensor-stack/xtl/archive/refs/tags/0.7.5.tar.gz --quiet -O - | tar zxf - --strip-components 1 -C xtl)
 (cd xtl; cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=${OUTPUT_DIR} . ; make install -j$(nproc) )
 
-(mkdir xeus && wget https://github.com/jupyter-xeus/xeus/archive/refs/tags/3.1.1.tar.gz --quiet -O - | tar zxf - --strip-components 1 -C xeus)
+#(mkdir xeus && wget https://github.com/jupyter-xeus/xeus/archive/refs/tags/3.1.1.tar.gz --quiet -O - | tar zxf - --strip-components 1 -C xeus)
+#(cd xeus; cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=${OUTPUT_DIR} . ; make install -j$(nproc) )
+(mkdir xeus && wget https://github.com/jupyter-xeus/xeus/archive/refs/tags/3.2.0.tar.gz --quiet -O - | tar zxf - --strip-components 1 -C xeus)
 (cd xeus; cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=${OUTPUT_DIR} . ; make install -j$(nproc) )
 
 (mkdir cppzmq && wget https://github.com/zeromq/cppzmq/archive/refs/tags/v4.8.1.tar.gz --quiet -O - | tar zxf - --strip-components 1 -C cppzmq)
@@ -15,3 +20,6 @@ OUTPUT_DIR=/opt/xeus3
 
 (mkdir -p xeus-zmq/build && wget https://github.com/jupyter-xeus/xeus-zmq/archive/refs/tags/1.1.0.tar.gz --quiet -O - | tar zxf - --strip-components 1 -C xeus-zmq)
 (cd xeus-zmq/build; cmake -DCMAKE_PREFIX_PATH=${OUTPUT_DIR} -DCMAKE_INSTALL_PREFIX=${OUTPUT_DIR} -DCMAKE_BUILD_TYPE=Release ..; make install -j$(nproc) )
+## iopub_welcome does not match with jupyter-console
+#(mkdir -p xeus-zmq/build && wget https://github.com/jupyter-xeus/xeus-zmq/archive/refs/tags/1.3.0.tar.gz --quiet -O - | tar zxf - --strip-components 1 -C xeus-zmq)
+#(cd xeus-zmq/build; cmake -DCMAKE_PREFIX_PATH=${OUTPUT_DIR} -DCMAKE_INSTALL_PREFIX=${OUTPUT_DIR} -DCMAKE_BUILD_TYPE=Release ..; make install -j$(nproc) )
